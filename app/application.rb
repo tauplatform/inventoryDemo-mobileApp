ab8e0aa7022d3780dcb9a9babdd0258cf629e248
vmusulainen <vmusulainen@gmail.com>
2017-03-03 08:30:39 +0300

    Toolbar is removed on iOS

--- app/application.rb
+++ app/application.rb
@@ -8,29 +8,12 @@ class AppApplication < Rho::RhoApplication
     # @default_menu = {}
 
     #To remove default toolbar uncomment next line:
-
-
-    if System::get_property('platform') == 'APPLE'
-        toolbarz = [
-          {:action => :back,    :icon => '/public/images/back_btn.png'},
-          {:action => :separator},
-          {:action => :home,    :icon => '/public/images/home_btn.png'},
-          {:action => "app/Settings/do_sync",    :icon => '/public/images/sync_btn.png'},
-          {:action => :refresh },
-          {:action => :options,    :icon => '/public/images/gears.png'}
-        ]
-      @@toolbar = toolbarz
-    else
-        @@toolbar = nil
-    end
+    @@toolbar = nil
 
     super
 
-
     # Uncomment to set sync notification callback to /app/Settings/sync_notify.
-     #Rho::RhoConnectClient.setObjectNotification("/app/Settings/sync_notify")
-     Rho::RhoConnectClient.setNotification('*', "/app/Settings/sync_notify", '')
-
-
+    #Rho::RhoConnectClient.setObjectNotification("/app/Settings/sync_notify")
+    Rho::RhoConnectClient.setNotification('*', "/app/Settings/sync_notify", '')
   end
 end
