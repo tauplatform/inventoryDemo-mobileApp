65627efec4669721797c5e0c536203813c2e9c9a
vmusulainen <vmusulainen@gmail.com>
2018-04-11 12:11:11 +0300

    wizard pages refactoring

--- app/Settings/controller.rb
+++ app/Settings/controller.rb
@@ -54,44 +54,6 @@ class SettingsController < Rho::RhoController
     scanner.friendlyName== Rho::Barcode.getDefault.friendlyName ? 'selected' : ''
   end
 
-  def login
-    @msg = @params['msg']
-    render :action => :login
-  end
-
-  def login_callback
-    errCode = @params['error_code'].to_i
-    if errCode == 0
-      Rho::WebView.navigate Rho::Application.startURI
-    else
-      if errCode == Rho::RhoError::ERR_CUSTOMSYNCSERVER
-        @msg = @params['error_message']
-      end
-
-      if !@msg || @msg.length == 0
-        @msg = Rho::RhoError.new(errCode).message
-      end
-      Rho::WebView.navigate (url_for :action => :login, :query => {:msg => @msg})
-    end
-  end
-
-  def do_login
-    if @params['login'] and @params['password']
-      begin
-        @@login = @params['login']
-        @@password = @params['password']
-        @response['headers']['Wait-Page'] = 'true'
-        render :action => :wait_login
-      rescue Rho::RhoError => e
-        @msg = e.message
-        render :action => :login
-      end
-    else
-      @msg = Rho::RhoError.err_message(Rho::RhoError::ERR_UNATHORIZED) unless @msg && @msg.length > 0
-      render :action => :login
-    end
-  end
-
   def logout
     Rho::RhoConnectClient.logout
     @msg = "You have been logged out."
@@ -117,13 +79,6 @@ class SettingsController < Rho::RhoController
     event = SyncEvent.create(data)
     event.notify_UI
     Rho::RhoConnectClient.doSync
-  end
-
-  def execute_login
-    #Rho::RhoConnectClient.login(@@login, @@password, (url_for :action => :login_callback))
-    @@login = nil
-    @@password = nil
-    Rho::WebView.navigate Rho::Application.startURI
   end
 
   def do_quit
