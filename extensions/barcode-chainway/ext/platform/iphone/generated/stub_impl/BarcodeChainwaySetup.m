5483c018178faf82f9ec554d90bb06dc7c55ba45
Alex Epifanov <aepifanov@tau-technologies.com>
2019-08-16 04:06:54 +0300

    update

--- extensions/barcode-chainway/ext/platform/iphone/generated/stub_impl/BarcodeChainwaySetup.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#include "common/app_build_capabilities.h"
-
-extern void Init_BarcodeChainway_API();
-
-void Init_BarcodeChainway()
-{
-    Init_BarcodeChainway_API();
-}
